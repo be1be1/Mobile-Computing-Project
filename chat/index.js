cd85a0ac7849712624ea96fb4fbecf99a27f5d65
Beibei <beibei@Beibeis-MacBook-Pro.local>
2017-04-05 16:18:58 +0800

    Mongoose Connect

--- chat/index.js
+++ chat/index.js
@@ -1,19 +1,59 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+	var app = require('express')();
+	var http = require('http').Server(app);
+	var io = require('socket.io')(http);
+	var mongoose = require('mongoose');
+	var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+	mongoose.connect('mongodb://localhost/activity', function(err){
+	    if (err) {
+	    	console.log(err);
+	    } else {
+	    	console.log('Connected to mongodb');
+	    }
+	});
 
-io.on('connection', function(socket){
-  console.log('new user comes in!');
-  socket.on('chat message', function(msg){
-    io.emit('show message', msg+' back');
-  });
-});
+	var actSchema = mongoose.Schema({
+	    type: String,
+	    class: Number,
+	    name: String,
+	    location: String,
+	    latitude: Number,
+	    longtitude: Number,
+	    tags: String,
+	    starttime: {type: Date, default: Date.now},
+	    endtime: {type: Date, default: Date.now},
+	    description: String
+	});
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
-});
+	var actModel = mongoose.model('ActData', actSchema);
+
+	app.get('/', function(req, res){
+	  res.sendFile(__dirname + '/index.html');
+	});
+
+	io.on('connection', function(socket){
+	  console.log('new user comes in!');
+	  socket.on('upload', function(data, callback){
+	  	console.log(data.type);
+	  	var newAct = new actModel({type:data.type, class:data.class});
+	  	newAct.save(function(err){
+	  		 if(err) throw err;
+	  		 io.emit('show message', ' Upload success!');
+	  	});
+	  });
+
+	  socket.on('download', function(data, callback){
+	    actModel.find({longtitude: {$gt: data.longtitude, $lt: data.longtitude+10}, 
+	     	            latitude: {$gt: data.latitude, $lt: data.latitude+10}}, 
+	     	            function(err, docs){
+	     	              if (err) throw err;
+	     	              console.log('sending documents');
+	     	              socket.emit('download_act', docs);
+	                    });
+	  });
+
+	});
+
+	http.listen(port, function(){
+	  console.log('listening on *:' + port);
+	});

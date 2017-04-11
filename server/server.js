f1a024c0bd385a657550e430fb363c40c797d0c8
be1be1 <beibei@Beibeis-MacBook-Pro.local>
2017-04-11 10:51:10 +0800

    Change the folder name

--- server/server.js
+++ server/server.js
@@ -1,47 +1,68 @@
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app).listen(1234);
-var io = require('socket.io').listen(server);
-var mongoose = require('mongoose');
+    var io = require('socket.io').listen(1234);
+    var jsonfile = require('jsonfile');
+	var mongoose = require('mongoose');
 
-console.log('Server Start.');
+	mongoose.connect('mongodb://localhost/activity', function(err){
+	    if (err) {
+	    	console.log(err);
+	    } else {
+	    	console.log('Connected to mongodb');
+	    }
+	});
 
-mongoose.connect('mongodb://localhost/activity', function(err){
-    if (err) {
-    	console.log(err);
-    } else {
-    	console.log('Connected to mongodb');
-    }
-});
+	var actSchema = mongoose.Schema({
+		type: String,
+	    tags: String,
+	    sub_type: Number,
+	    m_lat: Number,
+	    location: String,
+	    m_lon: Number,
+	    description: String,
+	    name: String,
+	    stime: Number,
+	    etime: Number,
+	});
 
-var actSchema = mongoose.Schema({
-    type: String,
-    class: Number,
-    name: String,
-    location: String,
-    latitude: Number,
-    longtitude: Number,
-    tags: String,
-    starttime: {type: Number, default: 0},
-    endtime: {type: Number, default: 0},
-    description: String
-});
+	var actModel = mongoose.model('ActData', actSchema);
 
-var actModel = mongoose.model('actData', actSchema);
+	io.sockets.on('connection', function(socket){
+	  console.log('Connected! New user comes in.');
 
-io.on('connection', function(socket){
-	console.log('wait for call');
-	socket.on('upload', function(data, callback){
-		console.log(data.type);
-	  	var newAct = new actModel({type:data.type, class:data.class});
+	  socket.on('upload', function(data, callback){
+	  	console.log(data);
+	  	var newAct = new actModel({type:data.type, tags:data.tags, sub_type:data.sub_type, m_lat:data.m_lat, location:data.location, m_lon:data.m_lon,
+	  	                           description: data.description, name: data.name, stime: data.stime, etime: data.etime});
 	  	newAct.save(function(err){
-	  		 if(err) throw err;
-	  		 io.emit('show message', ' Upload success!');
-	  	});
-	});
+	  		 var result_value = {"result": true};
+	  		 if(err) {
+	  		 	result_value = {"result": false};
+	  		 	throw err;
+	  		 }
 
-	socket.on('download', function(data, callback){
-		console.log('Downloading');
-		console.log(data.type);
+	  		 socket.emit('upload_result', result_value);
+	  	});
+	  });
+
+	  socket.on('download', function(data, callback){
+	    actModel.find({m_lon: {$gt: data.m_lon-1e-3, $lt: data.m_lon+1e-3}, 
+	     	           m_lat: {$gt: data.m_lat-1e-3, $lt: data.m_lat+1e-3},
+	     	           type: data.type,
+	     	           stime: {$lt: data.ntime},
+	     	           etime: {$gt: data.ntime}
+	     	           }, 
+
+	     	            function(err, docs){
+	     	              if (err) {
+	     	              	var result_value = {"num_result": 0};
+	     	              	socket.emit('download_data', result_value);
+	     	              	throw err;
+	     	              } else {
+	     	                console.log('sending documents');
+	     	                var kitty = {"num_result":1,
+	  						"result_data":docs};
+	     	                socket.emit('download_data', kitty);
+	     	              }
+	                    });
+	  });
+
 	});
-});
